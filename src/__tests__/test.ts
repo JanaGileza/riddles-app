20e9899f7dcd5683da16b860a7b4e7e5ebbb0620
JanaGileza <janam.gileze@gmail.com>
2025-03-28 16:13:30 -0400

    Converted project from javascript to typescript successfully!

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,23 +1,38 @@
 import { expect } from "@jest/globals";
-import { isCorrectAnswer, randomRiddles } from "../riddles";
+import { isCorrectAnswer, randomRiddles, Riddle } from "../riddles";
 
 describe("isCorrectAnswer() - Check if answer taken in matches answer given, case insensitive", () => {
   test("Given correct answer, returns true", () => {
     const correctAnswer = "correctAnswer";
-    const mockRiddle = [{ answer: correctAnswer }];
-    const isCorrect = isCorrectAnswer(correctAnswer, mockRiddle);
+    const mockRiddles: Riddle[]  = [{
+      answer: correctAnswer,
+      riddle: "",
+      id: 1,
+      hint: ""
+    }];
+    const isCorrect = isCorrectAnswer(correctAnswer, mockRiddles);
     expect(isCorrect).toBeTruthy();
   });
 
   test("Given correct answer with mismatching grammar, returns true", () => {
     const correctAnswer = "correctAnswer";
-    const mockRiddle = [{ answer: correctAnswer }];
-    const isCorrect = isCorrectAnswer("123CORreCtaN  sw123er", mockRiddle);
+    const mockRiddles: Riddle[] = [{
+      answer: correctAnswer,
+      riddle: "",
+      hint: "",
+      id: 0
+    }];
+    const isCorrect = isCorrectAnswer("123CORreCtaN  sw123er", mockRiddles);
     expect(isCorrect).toBeTruthy();
   });
 
   test("Given incorrect answer, returns false", () => {
-    const mockRiddles = [{ answer: "not-foundd" }];
+    const mockRiddles: Riddle[] = [{
+      answer: "not-fousndd",
+      riddle: "",
+      hint: "",
+      id: 0
+    }];
     const isCorrect = isCorrectAnswer("bad answer", mockRiddles);
     expect(isCorrect).toBe(false);
   });
--- src/riddles.ts
+++ src/riddles.ts
@@ -1,15 +1,22 @@
-function stripAnswer(answer) {
+type Riddle = {
+  riddle: string,
+  answer: string,
+  hint: string,
+  id: number 
+}
+
+function stripAnswer(answer: string) {
   const regexMatchingNonLetters = /[^[a-zA-Z]/gm;
   return answer.toString().toLowerCase().replace(regexMatchingNonLetters, "");
 }
 
-function isCorrectAnswer(attemptedAnswer, riddles) {
+function isCorrectAnswer(attemptedAnswer: string, riddles: Riddle[]) {
   if (attemptedAnswer.toString() === "") {
     return false;
   }
 
   const maybeRiddle = riddles.find(
-    ({ answer }) => stripAnswer(answer) === stripAnswer(attemptedAnswer)
+    (riddle) => stripAnswer(riddle.answer) === stripAnswer(attemptedAnswer)
   );
 
   return maybeRiddle !== undefined;
@@ -18,3 +25,4 @@ function isCorrectAnswer(attemptedAnswer, riddles) {
 function randomRiddles() {}
 
 export { isCorrectAnswer, randomRiddles };
+export type {Riddle}
