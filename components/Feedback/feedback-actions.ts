bbf2fb8a0b542f67ba02973551af6cada6eb4ac6
Jana Gileza <janam.gileza@gmail.com>
2025-09-03 19:17:43 -0400

    Add debugging for vercel testing

--- components/Feedback/feedback-actions.ts
+++ components/Feedback/feedback-actions.ts
@@ -14,9 +14,7 @@ const u = process.env.USER;
 const ap = process.env.A;
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: u,
     pass: ap,
@@ -32,6 +30,17 @@ const sendFeedbackEmail = async ({ name, email, body }: FeedbackData) => {
   });
 
   console.log("Message sent:", info.messageId);
+  try {
+    const info = await transporter.sendMail({
+      from: `${email}`,
+      to: fr,
+      subject: `New Feedback Received from ${name}`,
+      text: `Contact back at ${email}: ${body}`,
+    });
+    console.log("Message sent:", info);
+  } catch (err) {
+    console.error("Nodemailer error:", err);
+  }
 };
 
 function getResponse({ name, email, body }: FeedbackData) {
