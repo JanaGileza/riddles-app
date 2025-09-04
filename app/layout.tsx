558113c4689bed1c4eccbd00c8e3b560af659e0a
Jana M. Gileza <115898765+JanaGileza@users.noreply.github.com>
2025-09-03 20:11:26 -0400

    Develop (#11)
    * Quick fix
    
    * Hotfix/feedback form (#8)
    
    * Resolve outdated package-lock.json
    
    * Restoring feedback form changes lost in recent commits
    
    * Reimport nodemailer and @types/nodemailer
    
    * Change nodemailer email process
    
    * Hotfix/feedback form (#9)
    
    * Resolve outdated package-lock.json
    
    * Restoring feedback form changes lost in recent commits
    
    * Reimport nodemailer and @types/nodemailer
    
    * Change nodemailer email process
    
    * Change to host versus service nodemailer
    
    * Add debugging for vercel testing
    
    * Add potential vercel fix
    
    * Final fixes to feedback form
    
    * Fix issue of favicon not showing on browser
    
    * Fixing favicon issue
    
    * Fix favicon issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Random Riddle Generator",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
